af7fae8a8c6970c4f54e685a874b9a6e20f25729
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-11-06 13:08:40 -0600

    fhir safety checklist

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-medication-statement',
-  version: '1.3.4',
+  version: '1.3.6',
   summary: 'HL7 FHIR Resource - MedicationStatement',
   git: 'https://github.com/clinical-meteor/hl7-resource-medication-statement',
   documentation: 'README.md'
